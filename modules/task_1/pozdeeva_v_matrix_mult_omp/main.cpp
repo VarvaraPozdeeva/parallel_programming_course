dc6cf193c958ea54611b629e671d00afed6da11a
VarvaraPozdeeva <37691481+VarvaraPozdeeva@users.noreply.github.com>
2019-03-06 19:24:54 +0300

    syntax fix

--- modules/task_1/pozdeeva_v_matrix_mult_omp/main.cpp
+++ modules/task_1/pozdeeva_v_matrix_mult_omp/main.cpp
@@ -2,22 +2,19 @@
 #include <iostream>
 #include <ctime>
 #include <random>
-using namespace std;
 double* CreateRandMatrix(int size) {
     double* tmp = new double[size*size];
     for (int i = 0; i < size*size; i++)
-        tmp[i] = (rand() % 10000) / 1000.0f;
+        tmp[i] = (std::rand() % 10000) / 1000.0f;
     return tmp;
 }
-int PrintMatrix(double* A, int N) {
-    for (int i = 0; i<N*N; i += N)
-    {
+void PrintMatrix(double* A, int N) {
+    for (int i = 0; i < N*N; i += N) {
         for (int j = 0; j< N; j++)
-            cout << A[i + j] << " ";
-        cout << endl;
+            std::cout << A[i + j] << " ";
+        std::cout << std::endl;
     }
-    cout << endl;
-    return 1;
+    std::cout << std::endl;
 }
 void MultiMatrix(double* A, double* B, double* C, int n, int bSize) {
     for (int i = 0; i < bSize; ++i)
@@ -28,15 +25,15 @@ void MultiMatrix(double* A, double* B, double* C, int n, int bSize) {
 }
 void Foxs(double* A, double* B, double* C, int n, int q) {
     int bSize = n / q;
-    for(int i=0; i < q; i++)
-        for(int j=0; j < q; j++)
+    for (int i = 0; i < q; i++)
+        for (int j = 0; j < q; j++)
             for (int k = 0; k < q; k++) {
                 MultiMatrix(&A[(i*n+j)*bSize], &B[(j*n + k)*bSize], &C[(i*n + k)*bSize], n, bSize);
             }
 }
 int main(int argc, char** argv) {
     double *A, *B, *C;
-    int N=6, q=2;
+    int N = 6, q = 2;
     if (argc == 3) {
         N = atoi(argv[1]);
         q = atoi(argv[2]);
